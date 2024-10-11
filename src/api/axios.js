b0d2a62d86a7fd8563ec879ee361652c2ecf21f4
Juan Benjumea <juanpabate318@hotmail.com>
2024-10-11 02:12:05 -0500

    correcion en la ruta del backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://backend-28d1.onrender.com",
+  baseURL: "https://backend-28d1.onrender.com/api",
   withCredentials: true,
 });
